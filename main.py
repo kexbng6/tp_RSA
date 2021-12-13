adf8625bc77a0a8fb85c1065f8d7a9f6b1b619e6
Buscaape <dylan.soares.24@gmail.com>
2021-12-13 19:45:46 +0100

    Ajout de l'euclide étendu et de l'inverse modulaire

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+def euclide_etendu(a,b):
+    if (b==0):
+        return a,1,0
+    else:
+        (pgcd,u,v)= euclide_etendu(b, a%b)
+        return (pgcd,v,u-(a//b)*v)
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('RSA vie')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def modinv(a, m):
+    g, x, y = euclide_etendu(a, m)
+    if g != 1:
+        raise Exception('L\'inverse modulaire n\'existe pas')
+    else:
+        return x % m

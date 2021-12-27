4cf5922f8714aa8d5f77a7101aea1fb51ce53531
Kevin Bonga <elv-kevin-eric.bng@eduge.ch>
2021-12-27 15:40:58 +0100

    Suppression de l'import random(non-utilisé), Ajout fonction calculClePrivee, ajout de commentaires sur les fonctions-> manque intToUtf8(),parcoursListe()

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import codecs
 import math
-import random
 
 messages = [772942520,1093541194,154481546,876676876,1244840390,1259928347,1334120646,668279513,337542008,
 875782511,556980142,927450584,1120552795,505519659,1425707357,561927611,623792775,993148007,284705483,1199652702,
@@ -9,9 +9,16 @@ messages = [772942520,1093541194,154481546,876676876,1244840390,1259928347,13341
 725466080,815059584,1026780342,549262958,168593283,1152472931,246166200,672699349,434176708,332639250,1120552795,647161589,
 813987580]
 
+#var clePub,e correspondent à la clé publique (clePub=n)
 clePub=1452848381
 e=6991
 
+"""
+Fonction retournant deux nb prem dont le produit est n
+:param n -> clé publique
+:var moitier ->
+:return les 2 nb prem
+"""
 def trouverNbPremier(n):
     moitie = int(math.sqrt(n))
     for q in range(3, moitie):
@@ -20,6 +27,12 @@ def trouverNbPremier(n):
                 p= n//q
                 return p,q
 
+"""
+Fonction retournant le pgcd et les coef de Bézout de 2 nb entier relatif
+:param a -> 1er entier relatif:
+:param b -> 2ème entier relatif: 
+:return 3 var->le pgcd et les 2 coef de Bézout
+"""
 def euclide_etendu(a,b):
     if (b==0):
         return a,1,0
@@ -27,38 +40,58 @@ def euclide_etendu(a,b):
         (pgcd,u,v)= euclide_etendu(b, a%b)
         return (pgcd,v,u-(a//b)*v)
 
+"""
+Fonction qui calcule l'inv modulaire de 2 entiers
+:param a -> 1er entier relatif :
+:param m -> 2ème entier relatif :
+:var pgcd-> pgcd(a,m) retourné par la fct euclide_etendu()
+:var x -> coef de Bézout de a retourné par la fct euclide_etendu()
+:var y -> coef de Bézout de m retourné par la fct euclide_etendu()
+:return le coef de Bézout de a mod m
+"""
 def modinv(a, m):
-    g, x, y = euclide_etendu(a, m)
-    if g != 1:
-        raise Exception('L\'inverse modulaire n\'existe pas')
+    pgcd, x, y = euclide_etendu(a, m)
+    if pgcd != 1:
+        raise Exception('L\'inverse modulaire n\'existe pas')#les 2 nb ne sont pas premiers entre eux
     else:
         return x % m
 
+# def exporapide(a,n):
+#     if n==0:
+#         return 1
+#     b=exporapide(a,n//2)
+#     if (n%2)==1:
+#         return b*b*a
+#     else:
+#         return b*b
+
+"""
+Fonction permettant le calcul de puissance à l'aide de la récursivité basée sur l'algo d'exponentiation rapide
+:param a -> base
+:param n -> exposant
+:return -> a^n
+"""
 def exporapide(a,n):
-    if n==0:
-        return 1
-    b=exporapide(a,n//2)
-    if (n%2)==1:
-        return b*b*a
+    if n == 1:
+        return a
+    elif n%2==0:
+        return exporapide(a*a,n/2)
     else:
-        return b*b
+        return a*exporapide(a*a,(n-1)/2)
 
-p,q=trouverNbPremier(clePub)
-
-z=(p-1)*(q-1)
-
-pgcd,x,y=euclide_etendu(6991,z)
-
-d=x%z
-
-def parcoursListe(liste, exposant, clePublique):
-    messageFinal= ""
-    for i in liste:
-        m = pow(i, exposant, clePublique)
-        # code = m.to_bytes(4, 'little')
-        # mess = code.decode("UTF-8")
-        messageFinal+=intToUtf8(m)
-    print(messageFinal)
+"""
+Fonction calculant la clé privé à partir d'un couple de nb prem d'une cle publique
+:return -> clé privée
+"""
+def calculClePrivee():
+    p, q = trouverNbPremier(clePub)
+    z = (p - 1) * (q - 1) #indicatrice d'Euler (phi de (n))
+    ##variante 1->on utilise le coef de Bézout de l'exposant mod l'indicatrice d'Euler pour trouver la clé privée:
+    #pgcd, x, y = euclide_etendu(e, z)
+    #d = x % z
+    ##variante 2->on utilise l'inv mod de l'exposant et de l'indicatrice d'Euler pour trouver la clé privée
+    d = modinv(e, z)
+    return d
 
 def intToUtf8(m):
     m = str(hex(m)[2:]) # Prendre à partir du deuxiÃ¨me caractÃ¨re (aprÃ¨s le 0x)
@@ -66,4 +99,15 @@ def intToUtf8(m):
     m = codecs.decode(m,"hex").decode('utf-8') # DÃ©codage
     return m
 
-parcoursListe(messages,d,clePub)
+def parcoursListe(liste, exposant, clePublique):
+    messageFinal= ""
+    for i in liste:
+        #m = exporapide(i,exposant)%clePublique
+        m = pow(i, exposant, clePublique)
+        # code = m.to_bytes(4, 'little')
+        # mess = code.decode("UTF-8")
+        messageFinal+=intToUtf8(m)
+    print(messageFinal)
+
+if __name__ == '__main__':
+    parcoursListe(messages,calculClePrivee(),clePub)
